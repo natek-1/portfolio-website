e4c62fed82c4b0a31cd4ed73e037e8c8a935f14e
Nathan Gabriel <nategabrielk@icloud.com>
2024-06-05 20:10:55 -0400

    fix some bugs and ran migrations

--- portfolio/main/admin.py
+++ portfolio/main/admin.py
@@ -1,5 +1,5 @@
 from django.contrib import admin
-from models import Tag, Project, ProjectImage
+from .models import Tag, Project, ProjectImage
 
 class ProjectImageInline(admin.TabularInline):
     model = ProjectImage
--- portfolio/main/models.py
+++ portfolio/main/models.py
@@ -10,7 +10,7 @@ class Tag(models.Model):
 
 class Project(models.Model):
     title = models.CharField(max_length=200)
-    description = models.TextChoices()
+    description = models.TextField()
     tags = models.ManyToManyField(Tag, related_name="projects")
     link = models.URLField(max_length=200, blank=True)
 
